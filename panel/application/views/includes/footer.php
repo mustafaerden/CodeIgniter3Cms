cae0ad423417fd1d5b44823c8139293f8adbdc98
Mustafa Erden <mustafaerden87@gmail.com>
2018-07-31 21:19:42 +0300

    dashboard(anasayfa) parçalara ayrılarak blank page haline getirildi

---
+++ panel/application/views/includes/footer.php
@@ -0,0 +1,13 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+        <div class="clearfix">
+            <ul class="footer-menu pull-right">
+
+            </ul>
+            <div class="copyright pull-left">Tüm Hakları Saklıdır | mErden <?php echo date("Y") ?> &copy;</div>
+        </div>
+    </footer>
+</div>
+
+
+
