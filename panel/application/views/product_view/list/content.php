3718f415cfab8c9b3b75bfadd6b5bec98dd976fa
Mustafa Erden <mustafaerden87@gmail.com>
2018-08-02 00:03:51 +0300

    admin urunler sayfasinda tablo olusturuldu

--- panel/application/views/product_view/list/content.php
+++ panel/application/views/product_view/list/content.php
@@ -1,1 +1,34 @@
-<h3>product content</h3>
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürün Listesi</h4>
+    </div><!-- END column -->
+
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <th>#id</th>
+                    <th>Url</th>
+                    <th>Başlık</th>
+                    <th>Açıklama</th>
+                    <th>Durum</th>
+                    <th>İşlem</th>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>laptop-cantasi</td>
+                        <td>Laptop Çantası</td>
+                        <td>15 inc her markaya uygun laptop çantası</td>
+                        <td>Aktif</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil"></i> Düzenle</a>
+                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>
